cd5a880fba166d88367af2ab9bc053e70e0c5f8d
artur_kubek <artur.kubek.96@gmail.com>
2022-10-19 17:51:42 +0200

    main application

---
+++ src/main/java/pl/sda/copywykopy/CopyWykopyApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.copywykopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyWykopyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopyWykopyApplication.class, args);
+    }
+
+}
