d1946d9acea5190bd5eec1fb155cac0557b631a0
artur_kubek <artur.kubek.96@gmail.com>
2022-10-19 17:50:55 +0200

    post service class added

---
+++ src/main/java/pl/sda/copywykopy/service/PostService.java
@@ -0,0 +1,20 @@
+package pl.sda.copywykopy.service;
+
+import pl.sda.copywykopy.model.Post;
+
+import java.util.List;
+
+public class PostService {
+    public List<Post> findAll() {
+        return null;
+    }
+
+    public Post find(long id) {
+        return null;
+    }
+
+    public Post create(Post post) {
+        //TODO
+        return null;
+    }
+}
